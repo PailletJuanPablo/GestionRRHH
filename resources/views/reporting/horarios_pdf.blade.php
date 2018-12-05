f08667a691d3049b68bf4048ee11b52ab3f9298c
PailletJuanPablo <pailletjuanpablo@gmail.com>
2018-12-05 00:47:50 -0300

    Discard blank target

--- resources/views/reporting/horarios_pdf.blade.php
+++ resources/views/reporting/horarios_pdf.blade.php
@@ -1,30 +1,9 @@
-<!-- Bootstrap Material Datetime Picker Css -->
-<link href="{{ asset('resources/plugins/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css')}}" rel="stylesheet">
-</link>
-<!-- Google Fonts -->
-<link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
 <!-- Bootstrap Core Css -->
 <link href="{{ asset('resources/plugins/bootstrap/css/bootstrap.css')}}" rel="stylesheet">
-</link>
-<link href="{{ asset('resources/plugins/bootstrap-select/css/bootstrap-select.css')}}" rel="stylesheet">
-</link>
-<!-- Waves Effect Css -->
-<link href="{{ asset('resources/plugins/node-waves/waves.css')}}" rel="stylesheet">
-</link>
-<!-- Animation Css -->
-<link href="{{ asset('resources/plugins/animate-css/animate.css')}}" rel="stylesheet">
-</link>
-<!-- Morris Chart Css-->
-<link href="{{ asset('resources/plugins/morrisjs/morris.css')}}" rel="stylesheet">
 </link>
 <!-- Custom Css -->
 <link href="{{ asset('resources/css/style.css')}}" rel="stylesheet">
 </link>
-<!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
-<link href="{{ asset('resources/css/themes/theme-light-blue.css')}}" rel="stylesheet">
-</link>
-
 <table class="table table-striped">
     <thead>
        
